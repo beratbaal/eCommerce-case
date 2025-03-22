52c771e9b2d029c33430a981a148701249c371fe
Berat <64924845+beratbaal@users.noreply.github.com>
2025-03-22 23:00:45 +0300

    style fix

--- src/components/UI/ProductCard.tsx
+++ src/components/UI/ProductCard.tsx
@@ -31,9 +31,11 @@ const ProductCard: React.FC<ProductCardProps> = ({
       <div className="h-64 w-full overflow-hidden">
         <img src={image} alt={title} className="w-full h-full object-contain" />
       </div>
-      <div className="absolute inset-0 bg-black bg-opacity-70 opacity-0 group-hover:opacity-100 flex flex-col justify-center items-center text-white p-4 transition-opacity duration-300">
-        <h2 className="text-lg font-bold mb-2">{title}</h2>
-        <p className="text-gray-300 line-clamp-5 mb-2">{description}</p>
+      <div className="absolute inset-0 bg-black bg-opacity-70 opacity-0 group-hover:opacity-100 flex flex-col  justify-around items-center text-white p-4 transition-opacity duration-300">
+        <div>
+          <h2 className="text-lg font-bold mb-2">{title}</h2>
+          <p className="text-gray-300 line-clamp-4 mb-2">{description}</p>
+        </div>
         <div className="flex justify-between w-full">
           <div className="flex flex-col items-start w-full justify-start">
             <p className="text-yellow-400">
@@ -41,7 +43,7 @@ const ProductCard: React.FC<ProductCardProps> = ({
             </p>
             <span className="text-gray-400">({reviewCount} reviews)</span>
           </div>
-          <div>
+          <div className="w-full flex justify-end">
             <Button
               variant="addToCart"
               text="Sepete Ekle"
