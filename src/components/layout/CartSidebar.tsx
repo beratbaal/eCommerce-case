0bde46eb82e39496af844035f371b177ac4371b3
Berat <64924845+beratbaal@users.noreply.github.com>
2025-03-23 23:33:37 +0300

    ...

--- src/components/layout/CartSidebar.tsx
+++ src/components/layout/CartSidebar.tsx
@@ -27,7 +27,7 @@ const CartSidebar: React.FC = () => {
           isOpen ? "translate-x-0" : "translate-x-full"
         } transition-transform duration-300 z-50`}
       >
-        <div className="flex justify-between items-center p-4 border-b">
+        <div className="flex justify-between items-center  p-4 border-b">
           <h2 className="text-xl font-bold text-black">Sepet</h2>
           <FaTimes className="text-xl cursor-pointer" onClick={toggleSidebar} />
         </div>
@@ -38,7 +38,7 @@ const CartSidebar: React.FC = () => {
             state.items.map((item, index) => (
               <div
                 key={index}
-                className="flex justify-between items-center mb-4"
+                className="flex justify-between items-center mb-4 gap-x-2"
               >
                 <img
                   src={item.image}
